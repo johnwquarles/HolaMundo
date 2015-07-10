4e698ca9adfd365e7c4f9e45609c4bcbda41e997
johnwquarles <john.w.quarles@gmail.com>
2015-07-10 15:03:55 -0500

    translation works now on the first page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $http) {
 
-  
+  var language_table = {
+    "English" : "en",
+    "Spanish" : "es",
+    "French" : "fr",
+    "Albanian" : "sq",
+    "Chinese" : "zh",
+    "German" : "de",
+    "Greek" : "el",
+    "Japanese" : "ja",
+    "Russian" : "ru",
+    "Thai" : "th"
+  }
+
+  $scope.api_call = function(){
+    $scope.url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150710T143349Z.32ec879cf3d440f6.47a6650e41f0545ddef44afd94a20b8715ace660&lang=en-" + language_table[$scope.language] + "&text=" + $scope.query;
+    $http.get($scope.url).success(function(data){
+      $scope.data_object = data;
+    })
+    .error(function(data) {
+      $scope.error_data = data;
+    });
+  }
+
 })
-
-// .controller('ChatsCtrl', function($scope, Chats) {
-//   // With the new view caching in Ionic, Controllers are only called
-//   // when they are recreated or on app start, instead of every page change.
-//   // To listen for when this page is active (for example, to refresh data),
-//   // listen for the $ionicView.enter event:
-//   //
-//   //$scope.$on('$ionicView.enter', function(e) {
-//   //});
-
-//   $scope.chats = Chats.all();
-//   $scope.remove = function(chat) {
-//     Chats.remove(chat);
-//   }
-// })
-
-// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-//   $scope.chat = Chats.get($stateParams.chatId);
-// })
-
-// .controller('AccountCtrl', function($scope) {
-//   $scope.settings = {
-//     enableFriends: true
-//   };
-// });
